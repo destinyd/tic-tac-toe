8a49a03a35d73ed43e020ae25310f47385438a7a
destinyd <destinyd_war@163.com>
2019-06-30 13:10:53 +0800

    feat: improve output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io;
-
 macro_rules! numin {
     () =>{
         {
@@ -12,121 +10,173 @@ macro_rules! numin {
     };
 }
 
-fn main() {
-    println!("1 2 3 ");
-    println!("4 5 6 ");
-    println!("7 8 9 ");
-
-    let mut step = 0;
-    let mut user = 1;
-    // FIXME use array instead of vec
-    let mut points = [0; 9];
-
-    loop {
-        // 1 get point
-        let num: i32 = step_input(user);
-
-        // 2 put point
-        if !step_put(&mut points, num, user) { continue; }
-
-        // 3 output points
-        output(points);
-
-        // 4 Judge the winning or losing
-        if is_game_over(&mut points, user, step, num) {
-            println!("Game over");
-            break;
-        }
-
-        // 5 initial next
-        step_next(&mut user, &mut step);
-    }
+struct GameStruct {
+    input_num: usize,
+    step: i32,
+    user: i32,
+    points: [i32; 9],
 }
 
-fn output(points: [i32; 9]) {
-    let mut i = 0;
-    for data in points.iter() {
-        print!("{} ", data);
-        if i % 3 == 2 {
-            print!("\r\n");
-        }
-        i = i + 1;
-    }
+trait Game {
+    fn init(&self);
+    fn playing(&self) -> bool;
+    fn play(&mut self);
+
+    fn input(&mut self);
+    fn put_piece(&mut self) -> bool;
+    fn output(&self);
+    fn is_win(&self) -> bool;
+    fn next_step(&mut self);
 }
 
-fn is_win(points: &mut [i32; 9]) -> bool {
-    // 竖排
-    if points[0] == points[3] && points[0] == points[6] && points[0] > 0 {
-        return true;
-    }
-    if points[1] == points[4] && points[1] == points[7] && points[1] > 0 {
-        return true;
-    }
-    if points[2] == points[5] && points[2] == points[8] && points[2] > 0 {
-        return true;
+impl Game for GameStruct {
+    fn init(&self) {
+        println!("1 2 3 ");
+        println!("4 5 6 ");
+        println!("7 8 9 ");
     }
 
-    // 横排
-    if points[0] == points[1] && points[0] == points[2] && points[0] > 0 {
-        return true;
-    }
-    if points[3] == points[4] && points[3] == points[5] && points[3] > 0 {
-        return true;
-    }
-    if points[6] == points[7] && points[6] == points[8] && points[6] > 0 {
-        return true;
-    }
-
-    // 斜
-    if points[4] > 0 {
-        if points[0] == points[4] && points[0] == points[8] {
-            return true;
+    fn playing(&self) -> bool {
+        if self.step > 1 && self.input_num == 0 {
+            return false;
         }
-        if points[2] == points[4] && points[2] == points[6] {
-            return true;
+
+        if self.step <= 9 && !self.is_win() {
+            return true
         }
-    }
 
-    return false;
-}
-
-fn step_input(user: i32) -> i32 {
-    println!("请输入 user{} 放置位置，或输入 0 结束", user);
-    return numin!();
-}
-
-fn step_put(points: &mut [i32; 9] ,num: i32, user: i32) -> bool {
-    let index = num as usize - 1;
-    let point = points[index];
-
-    if point != 0 {
-        println!("位置 {} 已放置，请重新输入!", num);
         return false;
     }
-    points[index] = user;
-    return true;
-}
 
-fn is_game_over(points: &mut [i32; 9], user: i32, step: i32, num: i32) -> bool {
-    if step >= 5 - 1 {
-        if is_win(points) {
-            println!("user{} win!!!", user);
-            return true;
+    fn play(&mut self) {
+        println!("------step: {}--------", self.step);
+
+        self.input();
+
+        if self.put_piece() {
+            self.output();
+
+            if self.is_win() {
+                println!("user{} win!!!", self.user);
+            } else {
+                self.next_step();
+            }
         }
     }
 
-    if step == 8 || num == 0 {
-        return true
+    fn input(&mut self) {
+        println!("请输入 user{} 放置位置，或输入 0 结束", self.user);
+        self.input_num = numin!();
     }
 
-    return false;
+    fn put_piece(&mut self) -> bool {
+        let index = self.input_num - 1;
+        let point = self.points[index];
+
+        if point != 0 {
+            println!("位置 {} 已放置，请重新输入!", self.input_num);
+            return false;
+        }
+        self.points[index] = self.user;
+        return true;
+    }
+
+    fn output(&self) {
+        let mut i = 0;
+        for data in self.points.iter() {
+            match data {
+                1 => print!("|O"),
+                2 => print!("|X"),
+                _ => print!("| "),
+            };
+            if i % 3 == 2 {
+                print!("|\r\n");
+            }
+            i = i + 1;
+        }
+    }
+
+    fn is_win(&self) -> bool {
+        if self.step < 5 {
+            return false;
+        }
+
+        // 竖排
+        if self.points[0] == self.points[3] && self.points[0] == self.points[6] && self.points[0] > 0 {
+            return true;
+        }
+        if self.points[1] == self.points[4] && self.points[1] == self.points[7] && self.points[1] > 0 {
+            return true;
+        }
+        if self.points[2] == self.points[5] && self.points[2] == self.points[8] && self.points[2] > 0 {
+            return true;
+        }
+
+        // 横排
+        if self.points[0] == self.points[1] && self.points[0] == self.points[2] && self.points[0] > 0 {
+            return true;
+        }
+        if self.points[3] == self.points[4] && self.points[3] == self.points[5] && self.points[3] > 0 {
+            return true;
+        }
+        if self.points[6] == self.points[7] && self.points[6] == self.points[8] && self.points[6] > 0 {
+            return true;
+        }
+
+        // 斜
+        if self.points[4] > 0 {
+            if self.points[0] == self.points[4] && self.points[0] == self.points[8] {
+                return true;
+            }
+            if self.points[2] == self.points[4] && self.points[2] == self.points[6] {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn next_step(&mut self) {
+        if self.user == 1 {
+            self.user = 2;
+        } else {
+            self.user = 1;
+        }
+        self.step = self.step + 1;
+    }
 }
 
-fn step_next(user: &mut i32, step: &mut i32) {
-    if *user == 1 {
-        *user = 2;
-    } else {
-        *user = 1;
+struct GameBuilder {
+    input_num: usize,
+    step: i32,
+    user: i32,
+    points: [i32; 9],
+}
+
+impl GameBuilder {
+    fn new() -> GameBuilder {
+        GameBuilder { input_num: 0, step: 1, user: 1, points: [0; 9] }
     }
-    *step = *step + 1;
+
+    fn finalize(&self) -> GameStruct {
+        return GameStruct {
+            input_num: self.input_num,
+            step: self.step,
+            user: self.user,
+            points: self.points,
+        }
+    }
+}
+
+fn main() {
+    let mut game = GameBuilder::new().finalize();
+
+    game.init();
+
+    while game.playing() {
+        game.play();
+    }
+
+    println!("---------");
+    println!("Game Over");
 }
